c6a47d65e6c76f4240bbd9a5de0921a37678ad71
Sai Malla <x18149260@student.ncirl.ie>
2019-04-16 00:14:50 +0000

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,9 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
-config.serve_static_assets = true
+  
+  config.serve_static_assets = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -61,7 +63,7 @@ config.serve_static_assets = true
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "helpmate_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "maverick_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
